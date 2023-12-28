a10ca532618932c5319a52a3492d26acd2ebb319
Davichobits <davidruizdeveloper@gmail.com>
2023-12-28 14:59:57 -0500

    Pruebas con set interval - aún no se muestra la animación

--- skeleton.js
+++ skeleton.js
@@ -2,44 +2,61 @@ const canvas = document.querySelector('#canvas');
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d');
 ctx.imageSmoothingEnabled = false;
-// ctx.imageSmoothingQuality = 'high';
 
 const skeletonImg = new Image();
-skeletonImg.src = './sprites/Skeleton Idle.png';
-skeletonImg.onload = () => {
-  ctx.drawImage(skeletonImg, 0, 0, skeletonImg.width * 2, skeletonImg.height * 2);
-};
+skeletonImg.src = './sprites/Skeleton_Idle.png';
 
 let numberColumns = 11;
 let numberRows = 1;
-
-skeletonImg.width = skeletonImg.width * 2;
-skeletonImg.height = skeletonImg.height * 2;
-
 let frameWidth = skeletonImg.width / numberColumns;
 let frameHeight = skeletonImg.height / numberRows;
-
-// The sprite image frame starts from 0
 let currentFrame = 0;
 
-setInterval(function()
-{
-    // Pick a new frame
-    currentFrame++;
+skeletonImg.onload = () => ctx.drawImage(skeletonImg, 0, 0, skeletonImg.width, skeletonImg.height, 0, 0, skeletonImg.width * 4, skeletonImg.height * 4);
 
-    // Make the frames loop
-    let maxFrame = numberColumns * numberRows - 1;
-    if (currentFrame > maxFrame){
-        currentFrame = 0;
-    }
+//FIXME: corregir el siguiente codigo para que se muestre el sprite
+// setInterval(()=>{
+//   currentFrame++;
+//   console.log(currentFrame)
+//   // Make the frames loop
+//   let maxFrame = numberColumns * numberRows - 1;
+//   if (currentFrame > maxFrame){
+//       currentFrame = 0;
+//   }
+//   // Update rows and columns
+//   let column = currentFrame % numberColumns;
+//   let row = Math.floor(currentFrame / numberColumns);
+//   // Clear and draw
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   ctx.drawImage(skeletonImg, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 0, 0, frameWidth *4 , frameHeight*4);
+// }, 100);
 
-    // Update rows and columns
-    let column = currentFrame % numberColumns;
-    let row = Math.floor(currentFrame / numberColumns);
 
-    // Clear and draw
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(skeletonImg, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 10, 30, frameWidth, frameHeight);
+//FIXME: corregir el siguiente codigo para que se muestre el sprite
+// const animation = () => {
+//   currentFrame++;
+//   console.log('animation')
 
-//Wait for next step in the loop
-}, 100);
+//   // Make the frames loop
+//   let maxFrame = numberColumns * numberRows - 1;
+//   if (currentFrame > maxFrame){
+//       currentFrame = 0;
+//   }
+
+//   // Update rows and columns
+//   let column = currentFrame % numberColumns;
+//   let row = Math.floor(currentFrame / numberColumns);
+
+//   // Clear and draw
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   ctx.drawImage(skeletonImg, column * frameWidth, row * frameHeight, frameWidth, frameHeight, 0, 0, frameWidth *4 , frameHeight*4);
+//   requestAnimationFrame(animation);
+// }
+// animation();
+
+//TODO: Documentar
+// skeletonImg.addEventListener('load', () => {
+//     ctx.drawImage(skeletonImg, 0, 0);
+// });
+
+// skeletonImg.onload = () => ctx.drawImage(skeletonImg, 0, 0, skeletonImg.width, skeletonImg.height, 0, 0, skeletonImg.width * 4, skeletonImg.height * 4);
